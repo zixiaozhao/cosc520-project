3ff07984bd63c01ebed3467ce5f308a75bfe2b8b
BMCFADS <101163132+bmcfads@users.noreply.github.com>
2022-11-26 14:09:03 -0800

    Convert index.js to module

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,30 +1,18 @@
-// TODO: Import naive.js and fft.js
-// TODO: Validate user inputs
+import * as naive from "./gfg-naive.js";
+// import * as fft from "./fft.js";
 
-function printPolynomial(poly) {
-  var str = "";
-
-  for (var i = 0; i < poly.length; i++) {
-    poly[i] = poly[i] || 0;
-
-    if (i === 0) {
-      str =  " " + poly[i];
-    } else if (i === 1) {
-      str = poly[i] + "x +" + str;
-    } else {
-      str = poly[i] + "x^" + i + " + " + str;
-    }
-  }
-
-  return str;
-}
+// Add event listener for the multiply button
+document
+  .getElementById("multiply-btn")
+  .addEventListener("click", multiplyPolynomials);
 
 function multiplyPolynomials() {
-  var inputOne = document.getElementsByName('polyOne[]');
-  var inputTwo = document.getElementsByName('polyTwo[]');
+  var inputOne = document.getElementsByName("polyOne[]");
+  var inputTwo = document.getElementsByName("polyTwo[]");
   var polyOne = [];
   var polyTwo = [];
-  
+
+  // Add the polynomial coefficients to an array.
   for (var i = 0; i < inputOne.length; i++) {
     polyOne.push(inputOne[i].value);
   }
@@ -33,18 +21,39 @@ function multiplyPolynomials() {
     polyTwo.push(inputTwo[i].value);
   }
 
+  // Reverse coefficient order so that it matches the expected inputs for the
+  // polynomial multiplication algorithms.
   polyOne.reverse();
   polyTwo.reverse();
 
-  // TODO: Call naive and FFT algorithms
-  // naive = multiplyPolynomialsNaive();
-  // fft = multiplyPolynomialsFFT();
-  var naive = polyOne;
-  var fft = polyTwo;
+  // Multiply the polynomials.
+  var naiveArr = naive.multiply(polyOne, polyTwo);
+  // var fftArr = fft.multiply(polyOne, polyTwo);
+  var fftArr = polyTwo;
 
-  var naiveOut = printPolynomial(naive);
-  var fftOut = printPolynomial(fft);
+  // Convert results from a coefficient array to an output string.
+  var naiveStr = printPolynomial(naiveArr);
+  var fftStr = printPolynomial(fftArr);
 
-  document.getElementById("naive-output").innerHTML = naiveOut;
-  document.getElementById("fft-output").innerHTML = fftOut;
+  // Display the output string in the browser window.
+  document.getElementById("naive-output").innerHTML = naiveStr;
+  document.getElementById("fft-output").innerHTML = fftStr;
+}
+
+function printPolynomial(poly) {
+  var str = "";
+
+  for (var i = 0; i < poly.length; i++) {
+    poly[i] = poly[i] || 0;
+
+    if (i === 0) {
+      str = " " + poly[i];
+    } else if (i === 1) {
+      str = poly[i] + "x +" + str;
+    } else {
+      str = poly[i] + "x^" + i + " + " + str;
+    }
+  }
+
+  return str;
 }
