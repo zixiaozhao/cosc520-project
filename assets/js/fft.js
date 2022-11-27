44c3921423f6e77780b69b77c3dfef4f93d1dc1b
BMCFADS <101163132+bmcfads@users.noreply.github.com>
2022-11-26 18:18:09 -0800

    Create fft.js

---
+++ assets/js/fft.js
@@ -0,0 +1,11 @@
+/*
+  This is a Fast Fourier Transform (FFT) implementation of polynomial multiplication. 
+
+  The JavaScript implementation found at GeeksforGeeks was used as a starting 
+  point. Some modifications were made as required for the associated project.
+
+  Source: https://www.geeksforgeeks.org/fast-fourier-transformation-poynomial-multiplication/
+  Code Author: phasing17
+  Copyright: https://www.geeksforgeeks.org/copyright-information/
+  Licence: https://creativecommons.org/licenses/by-sa/2.0/
+*/
