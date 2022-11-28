80c4fdf6a4a0a66da18847eedcc6fec150fd1bad
Zixiao <61948711+zixiaozhao@users.noreply.github.com>
2022-11-27 17:53:03 -0800

    added function for complex number

--- assets/js/fft.js
+++ assets/js/fft.js
@@ -9,3 +9,31 @@
   Copyright: https://www.geeksforgeeks.org/copyright-information/
   Licence: https://creativecommons.org/licenses/by-sa/2.0/
 */
+class complex{
+    constructor(a, b = 0){
+        this.x = a;
+        this.y = b;
+    }
+}
+ 
+function product(a, b){
+    let c =new complex(0, 0);
+    c.x = a.x * b.x - a.y * b.y
+    c.y = a.x * b.y + b.x * a.y
+    return c
+}
+ 
+function sum(a, b){
+    let c = new complex(0, 0);
+    c.x = a.x + b.x
+    c.y = a.y + b.y
+    return c
+}
+ 
+function difference(a, b){
+    let c =new complex(0, 0);
+    c.x = a.x - b.x
+    c.y = a.y - b.y
+    return c
+}
+ 
