95c05588e00491c1594f9fed33671d77744d246e
Zixiao <61948711+zixiaozhao@users.noreply.github.com>
2022-11-30 22:28:06 -0800

    Update ComplexNumber.js

--- assets/js/ComplexNumber.js
+++ assets/js/ComplexNumber.js
@@ -1,56 +1,48 @@
+/*This work reuse some of the code and ideas from this github repo, https://github.com/infusion/Complex.js/blob/master/complex.js 
+* the repo is under the MIT licenses
+* @license Complex.js v2.1.1 12/05/2020
+*
+* Copyright (c) 2020, Robert Eisele (robert@xarg.org)
+* Dual licensed under the MIT or GPL Version 2 licenses.
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy of
+* this software and associated documentation files (the "Software"), to deal in
+* the Software without restriction, including without limitation the rights to
+* use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+* the Software, and to permit persons to whom the Software is furnished to do so,
+* subject to the following conditions:
+* - The above copyright notice and this permission notice shall be included in
+*   all copies or substantial portions of the Software.
+* - The Software is provided "as is", without warranty of any kind, express or
+*   implied, including but not limited to the warranties of merchantability,
+*   fitness for a particular purpose and noninfringement. In no event shall the
+*   authors or copyright holders be liable for any claim, damages or other
+*   liability, whether in an action of contract, tort or otherwise, arising from,
+*   out of or in connection with the Software or the use or other dealings in the
+*   Software.
+ * 
+ */
+
+
 export default class ComplexNumber {
-  constructor({ re = 0, im = 0 } = {}) {
+  constructor ({ re = 0, im = 0 } = {}) {
     this.re = re;
     this.im = im;
   }
 
-  add(a) {
+  add = (a) => new ComplexNumber({re: this.re + a.re, im: this.im + a.im})
+  subtract = (a) => new ComplexNumber({re: this.re - a.re, im: this.im - a.im});
+  multiply = (a) => new ComplexNumber({re: this.re * a.re - this.im * a.im,im: this.re * a.im + this.im * a.re})
+  conjugate = (a) => new ComplexNumber({re: a.re, im: -1 * a.im,});
 
+  divide(x) {
+    var a = this.re;
+    var b = this.im;
+    var c = x.re;
+    var d = x.im;
     return new ComplexNumber({
-      re: this.re + a.re,
-      im: this.im + a.im,
+      re: (a * c + b * d) / (c * c + d * d),
+      im:  (b * c - a * d) / (c * c + d * d),
     });
   }
-
-  subtract(a) {
-
-    return new ComplexNumber({
-      re: this.re - a.re,
-      im: this.im - a.im,
-    });
-  }
-
-  multiply(a) {
-
-    return new ComplexNumber({
-      re: this.re * a.re - this.im * a.im,
-      im: this.re * a.im + this.im * a.re,
-    });
-  }
-
-  divide(a) {
-
-    // Get divider conjugate.
-    const dividerConjugate = this.conjugate(a);
-
-    // Multiply dividend by divider's conjugate.
-    const finalDivident = this.multiply(a);
-
-    // Calculating final divider using formula (a + bi)(a − bi) = a^2 + b^2
-    const finalDivider = (a.re ** 2) + (a.im ** 2);
-    return new ComplexNumber({
-      re: finalDivident.re / finalDivider,
-      im: finalDivident.im / finalDivider,
-    });
-  }
-
-  conjugate(a) {
-
-    return new ComplexNumber({
-      re: a.re,
-      im: -1 * a.im,
-    });
-  }
-
-
 }
